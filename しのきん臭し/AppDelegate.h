eb4f523f4546e66cfdab18ffa13d20cb70990418
小泉心之介 <koizumikokoronosuke@koizumitakeshi-no-MacBook-Air.local>
2015-04-17 18:40:16 +0900

    Initial Commit

---
+++ しのきん臭し/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  しのきん臭し
+//
+//  Created by 小泉心之介 on 2015/04/17.
+//  Copyright (c) 2015年 小泉心之介. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ しのきん臭し/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  しのきん臭し
+//
+//  Created by 小泉心之介 on 2015/04/17.
+//  Copyright (c) 2015年 小泉心之介. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
